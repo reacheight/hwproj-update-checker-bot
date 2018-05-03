d191cad2ab867427b7bac70571c59603b2b1da6e
reacheight <reacheight8@gmail.com>
2018-05-03 11:50:44 +0300

    code refactored for better deploing on heroku

--- bot.py
+++ bot.py
@@ -1,32 +1,33 @@
 import telebot
-import config
 import scraper
 import requests
 import schedule
 import time
+import config
+
+current_accepted = 0
+current_unaccepted = 0
 
 bot = telebot.TeleBot(config.token)
 
 
 def get_updates():
+    global current_accepted, current_unaccepted
+
     try:
-        new_accepted_count, new_unaccepted_count = scraper.task_count()
+        new_accepted, new_unaccepted = scraper.task_count()
     except requests.RequestException:
         bot.send_message(config.my_id, 'request error occurred')
         return
 
-    with open(config.data_path, 'r') as data:
-        old_accepted_count, old_unaccepted_count = [int(number) for number in data.read().split()]
-
-    with open(config.data_path, 'w') as data:
-        data.write(f'{new_accepted_count} {new_unaccepted_count}')
-
-    accepted = new_accepted_count - old_accepted_count
-    unaccepted = new_unaccepted_count - old_unaccepted_count
+    accepted = new_accepted - current_accepted
+    unaccepted = new_unaccepted - current_unaccepted
     if accepted != 0 or unaccepted != 0:
         bot.send_message(config.my_id, f"UPDATES:\n"
                                        f"{accepted}  новых принятых задач.\n"
                                        f"{unaccepted} новых непринятых задач.")
+
+    current_accepted, current_unaccepted = new_accepted, new_unaccepted
 
 
 schedule.every(2).hours.do(get_updates)
