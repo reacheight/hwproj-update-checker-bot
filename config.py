e466eb16790c5fdc9af9ae45c433708f4b8101af
reacheight <reacheight8@gmail.com>
2018-09-12 23:52:23 +0300

    updated course url

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ token = os.environ.get('BOT_TOKEN')
 redis_url = os.environ.get('REDIS_URL')
 
 my_name = os.environ.get('MY_NAME')
-course_url = 'http://hwproj.me/courses/31'
+course_url = 'http://hwproj.me/courses/31/terms/3'
 logs_channel = '@hwporj_checker_logs'
 
 accepted_db_key = 'accepted'
